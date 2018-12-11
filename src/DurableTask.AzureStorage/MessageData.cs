5f049ae1d2da9d58e758dfc0a571250b42090c5d
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-12-12 07:13:10 +0900

    Revert "Adding Serialize mechanizm of Distributed Tracing"
    This reverts commit e20d9e7577189cb6541094b6214d171dc8b25862.

--- src/DurableTask.AzureStorage/MessageData.cs
+++ src/DurableTask.AzureStorage/MessageData.cs
@@ -75,12 +75,6 @@ namespace DurableTask.AzureStorage
         [DataMember]
         public int Episode { get; private set; }
 
-        /// <summary>
-        /// TraceContext for correlation.
-        /// </summary>
-        [DataMember]
-        public TraceContextStore TraceContextStore { get; set; }
-
         internal string Id => this.OriginalQueueMessage?.Id;
 
         internal string QueueName { get; set; }
